f19d6fdd2a02046064047c2ca1e3af34aee163a0
Phil <github@exclsr.com>
2012-10-29 18:04:41 -0700

    Refactor: Put authentication into own module
    Move the authentication code out of server.js
    and into ./lib/auth.js. Feel free to rename
    and move this somewhere else as we become more
    accustomed to nodejs naming conventions.
    
    Change-Id: Ib7e1b57dcbfe3432ec21cdab7d363fe12cdecf4e

--- server.js
+++ server.js
@@ -1,48 +1,6 @@
 var express  = require('express');
 var request  = require('request');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
-
-var users = [
-    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
-  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
-];
-
-function findById(id, fn) {
-  var idx = id - 1;
-  if (users[idx]) {
-    fn(null, users[idx]);
-  } else {
-    fn(new Error('User ' + id + ' does not exist'));
-  }
-}
-
-function findByUsername(username, fn) {
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-    if (user.username === username) {
-      return fn(null, user);
-    }
-  }
-  return fn(null, null);
-}
-
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  findById(id, function (err, user) {
-    done(err, user);
-  });
-});
-
+var auth = require('./lib/auth.js');
 
 var app = express();
 var localfile = express.static(__dirname + '/app');
@@ -57,36 +15,12 @@ app.configure(function() {
   app.use(express.session({ secret: 'what? ok!' }));
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
-  app.use(passport.initialize());
-  app.use(passport.session());
+  app.use(auth.initialize());
+  app.use(auth.session());
   app.use(app.router);
   app.use(localfile);
 });
 
-
-// Use the LocalStrategy within Passport.
-//   Strategies in passport require a `verify` function, which accept
-//   credentials (in this case, a username and password), and invoke a callback
-//   with a user object.  In the real world, this would query a database;
-//   however, in this example we are using a baked-in set of users.
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      
-      // Find the user by username.  If there is no user with the given
-      // username, or the password is not correct, set the user to `false` to
-      // indicate failure and set a flash message.  Otherwise, return the
-      // authenticated `user`.
-      findByUsername(username, function(err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
-        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
-        return done(null, user);
-      })
-    });
-  }
-));
 
 // app.all('*', requireAuthentication)
 // app.all('*', loadUser);
@@ -94,7 +28,6 @@ passport.use(new LocalStrategy(
 var usersExist = function() {
 	return false;
 };
-
 
 app.get("/", function(req, res) {
 	if (!usersExist()) {
@@ -105,25 +38,12 @@ app.get("/", function(req, res) {
 	}
 });
 
-app.get("/data/what", ensureAuthenticated, function(req, res) {
+app.get("/data/what", auth.ensure, function(req, res) {
 	res.send(req.user);
 });
 
-app.post('/login', 
-	passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
-	function(req, res) {
-    	res.redirect('/');
-	});
-
-
-// Simple route middleware to ensure user is authenticated.
-//   Use this route middleware on any resource that needs to be protected.  If
-//   the request is authenticated (typically via a persistent login session),
-//   the request will proceed.  Otherwise, the user will be redirected to the
-//   login page.
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
-}
+app.post('/login', auth.authenticate('/login'), function(req, res) {
+   	res.redirect('/');
+});
 
 app.listen(80);
