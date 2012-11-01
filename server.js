9ca348bc4a87d3b00679a4281f29c81f19646727
Phil <github@exclsr.com>
2012-10-31 19:05:52 -0700

    Refactor authentication to make more sense
    Put all the passport stuff inside the auth library,
    and make an auth middleware piece that is somewhat
    understandable.
    
    Change-Id: I0464b125b9ff81ee5449930350801613048ed157

--- server.js
+++ server.js
@@ -5,21 +5,26 @@ var db = require('./lib/dataAccess.js').instance();
 
 var app = express();
 var localfile = express.static(__dirname + '/app');
+
+// Configure auth 
+auth.usernameField('email');
+auth.passwordField('password');
+
 // configure Express
 app.configure(function() {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.logger());
-  app.use(express.cookieParser());
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.session({ secret: 'what? ok!' }));
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(express.logger());
+	app.use(express.cookieParser());
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.session({ secret: 'what? ok!' }));
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
-  app.use(auth.initialize());
-  app.use(auth.session());
-  app.use(app.router);
-  app.use(localfile);
+	app.use(auth.initialize());
+	app.use(auth.session());
+	app.use(app.router);
+	app.use(localfile);
 });
 
 
@@ -44,58 +49,53 @@ app.get("/", function(req, res) {
 	}
 });
 
-// Authentication
-// app.post('/login', auth.authenticate('/login'), function(req, res) {
-//     res.redirect('/');
-// });
+// Authentication. This defines what we send
+// back to clients that want to authenticate
+// with the system.
+var authMiddleware = function(req, res, next) {
 
-app.post('/login', function(req, res, next) {
-  auth.authenticate(function(err, user, info) {
-    if (err) { 
-      // TODO: Test
-      // return next(err) 
-      return res.send(401, err); 
-    }
-    if (!user) { 
-      // Invalid login data
-      return res.send(401, "Unauthorized"); 
-    }
-    req.logIn(user, function(err) {
-      if (err) { 
-        // TODO: What is this? Test.
-        //return next(err); 
-        return res.send(401, err);         
-      }
-      // Successful login
-      return res.send(200, "Login successul");
-    });
-  })(req, res, next);
-});
+	var success = function() {
+		res.send(200, "Login successul");
+	};
+
+	var failure = function(error) {
+		console.log(error);
+		res.send(401, "Unauthorized"); 
+	};
+
+	// The auth library provides middleware that
+	// calls 'success' or 'failure' in the appropriate
+	// login situation.
+	var middleware = auth.authenticate(req, success, failure);
+	middleware(req, res, next);
+};
+
+app.post('/login', authMiddleware);
 
 
 // Data API: First-time configuration
 app.put("/data/initialize", function(req, res) {
-  var data = req.body;
+	var data = req.body;
 
-  var user = {
-    email: data.email,
-    id: db.users.count() + 1
-  };
-  var password = data.password;
+	var user = {
+		email: data.email,
+		id: db.users.count() + 1
+	};
+	var password = data.password;
 
-  db.users.add(
-    user,
-    password,
-    function() {
-      // Success.
-      // TODO: ...
-      res.send("Ok!");
-    },
-    function(error) {
-      // Fail.
-      res.send(500, error);
-    }
-  );
+	db.users.add(
+		user,
+		password,
+		function() {
+			// Success.
+			// TODO: ...
+			res.send("Ok!");
+		},
+		function(error) {
+			// Fail.
+			res.send(500, error);
+		}
+	);
 });
 
 // Data API: Protected by authorization system
