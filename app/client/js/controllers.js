bdf01fedacb08aa989f1be2751031b1af379b13a
Phil <github@exclsr.com>
2012-11-02 14:58:58 -0700

    Initialize: Log in after creating admin user
    It's convenient to log in after creating the
    initial user, so let's do that.
    
    Change-Id: I037b7fd7ea2d8f67d74fd8c56b11f8e2fa06ccf8

--- app/client/js/controllers.js
+++ app/client/js/controllers.js
@@ -8,23 +8,34 @@ HomeCtrl.$inject = ['$scope'];
 
 
 function LoginCtrl($scope, $location, $http) {
-	
+
 	$scope.login = function() {
+		var success = function(data, status, headers, config) {
+			$scope.message = "Success!";
+		};
 
-		var xsrf = $.param($scope.user);
+		var failure = function(data, status, headers, config) {
+			$scope.message = "Sad. :-("
+		};
 
-		var res = $http({
-			method: 'POST',
-			url: '/login',
-			data: xsrf,
-			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-		})
-			.success(function(data, status, headers, config) {
-				$scope.message = "Success!";
-			})
-			.error(function(data, status, headers, config) {
-				$scope.message = "Sad. :-("
-    		});
+		// TODO: This should probably be inside a resource, or whatever
+		// the things like $scope and $location are called in Angular.
+		// Refactor this when you're in the mood to learn, future-self.
+		var login = function (user, success, failure) {
+			var xsrf = $.param(user);
+			var request = {
+				method: 'POST',
+				url: '/login',
+				data: xsrf,
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			};		
+
+			$http(request)
+			.success(success)
+			.error(failure);
+		};
+
+		login($scope.user, success, failure);
 	};
 }
 LoginCtrl.$inject = ['$scope', '$location', '$http'];
@@ -37,13 +48,45 @@ function InitializeCtrl($scope, $http, $location) {
 	$scope.initialize = function() {
 		$scope.user.name = "Administrator";
 
-		var res = $http.put('/data/initialize', $scope.user)
-			.success(function(data, status, headers, config) {
+		// TODO: This code is duplicated here because I don't know
+		// "the right way" to share code among controlllers in 
+		// Angular, yet. Well, ok, we probably need to make
+		// a service.
+		var login = function (user, success, failure) {
+			var xsrf = $.param(user);
+			var request = {
+				method: 'POST',
+				url: '/login',
+				data: xsrf,
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			};		
+
+			$http(request).success(success).error(failure);
+		};
+
+		var initializeSuccess = function(data, status, headers, config) {
+			// After initialization is a success, log in.
+			var loginSuccess = function(data, status, headers, config) {
 				$location.path('/');
-			})
-			.error(function(data, status, headers, config) {
+			};
+
+			var loginFailure = function(data, status, headers, config) {
 				// TODO: Display error.
-    		});
+				console.log("Failure to log in after initialization.");
+			};
+
+			login($scope.user, loginSuccess, loginFailure);
+		};
+
+		var initializeFailure = function(data, status, headers, config) {
+			// TODO: Display error.
+			console.log("Failure to initialize");
+			console.log(JSON.stringify(data));
+		};
+
+		$http.put('/data/initialize', $scope.user)
+		.success(initializeSuccess)
+		.error(initializeFailure);
 	};
 }
 InitializeCtrl.$inject = ['$scope', '$http', '$location'];
