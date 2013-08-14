e4482e6404d0d0b1b2d2e2efc21da89512f2a4e5
Phil <github@exclsr.com>
2013-08-14 15:55:53 -0700

    Refactor: Database: Put views in own file

--- app/lib/dataAccess.js
+++ app/lib/dataAccess.js
@@ -4,122 +4,7 @@ var DIGEST_ENCODING = 'hex';
 var crypto = require('crypto');
 var uuid = require('node-uuid');
 var nano = require('nano');
-
-var createViews = function(database, callback) {
-	var designDocs = [];
-
-	var maybeReady = function (designDoc) {
-		var areAllDocsReady = true;
-
-		designDoc.isReady = true;
-		designDocs.forEach(function (doc) {
-			if (!doc.isReady) {
-				areAllDocsReady = false;
-			}
-		});
-
-		if (areAllDocsReady) {
-			callback();
-		}
-	};
-
-
-	var usersDesignDoc = {
-		url: '_design/users',
-		body: 
-		{
-			version: "1.0.0",
-			language: "javascript",
-			views: {
-				byEmail: {
-					map: function(doc) {
-						if (doc.type === "user" && doc.email) {
-							emit(doc.email, doc);
-						}
-					}
-				},
-				byId: {
-					map: function (doc) {
-						if (doc.type === "user" && doc.id) {
-							emit(doc.id, doc);
-						}
-					}
-				}
-			}
-		}
-	};
-	designDocs.push(usersDesignDoc);
-
-	var passwordsDesignDoc = {
-		url: '_design/passwords',
-		body: 
-		{
-			version: "1.0.1",
-			language: "javascript",
-			views: {
-				byId: {
-					map: function (doc) {
-						if (doc.type === "password" && doc.userId) {
-							emit(doc.userId, doc);
-						}
-					}
-				}
-			}
-		}
-	};
-	designDocs.push(passwordsDesignDoc);
-
-
-	var saveDesignDocs = function () {
-		var saveDoc = function (doc) {
-			database.insert(doc.body, doc.url, function (err, body) {
-				if (err && err['status-code'] === 409) {
-					// document conflict (always happens if doc exists)
-					database.get(doc.url, function (err, body) {
-						if (err) {
-							callback(err);
-						}
-						else {
-							doc.body._id = body._id;
-							doc.body._rev = body._rev;
-							saveDoc(doc);
-						}
-					});
-				}
-				else if (err) {
-					callback(err);
-				}
-				else {
-					maybeReady(doc);
-				}
-			});
-		};
-
-		// Save our design doc if it doesn't exist or if
-		// the version in the database is different from
-		// the one we have in the code.
-		designDocs.forEach(function (doc) {
-			database.get(doc.url, function (err, body) {
-				if (err && err['status-code'] === 404) {
-					saveDoc(doc);	
-				}
-				else if (err) {
-					callback(err);
-				}
-				else {
-					if (body.version === doc.body.version) {
-						// Up to date.
-						maybeReady(doc);
-					}
-					else {
-						saveDoc(doc);
-					}
-				}
-			});
-		});
-	}(); // closure
-};
-
+var views = require('./views.js');
 
 // TODO: This should be in its own module probably.
 var hashPassword = function(password, salt) {
@@ -182,7 +67,7 @@ var couch = function() {
 				throw (err);
 			}
 			else if (exists) {
-				createViews(database, callback);
+				views.create(database, callback);
 			}
 			else {
 				createDatabase(function (err) {
@@ -191,7 +76,7 @@ var couch = function() {
 						callback(err);
 					}
 					else {
-						createViews(database, callback);		
+						views.create(database, callback);		
 					}
 				});
 			}
