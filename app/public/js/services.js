df94c88248dc01c885d82bd4367a1f41a9fe8f71
Phil <github@exclsr.com>
2013-08-15 13:04:57 -0700

    Add session service
    Add a session service to our Angular app,
    basically a singleton we can pass around
    that doesn't expire for 12 hours (on a
    rolling basis).

--- app/public/js/services.js
+++ app/public/js/services.js
@@ -1,9 +1,98 @@
 'use strict';
 
+// Added to make dates format to ISO8601 across browsers
+// h/t: http://stackoverflow.com/a/2218874/124487
+Date.prototype.toJSON = function (key) {
+    function f(n) {
+        // Format integers to have at least two digits.
+        return n < 10 ? '0' + n : n;
+    }
+
+    return this.getUTCFullYear()   + '-' +
+         f(this.getUTCMonth() + 1) + '-' +
+         f(this.getUTCDate())      + 'T' +
+         f(this.getUTCHours())     + ':' +
+         f(this.getUTCMinutes())   + ':' +
+         f(this.getUTCSeconds())   + '.' +
+         f(this.getUTCMilliseconds())   + 'Z';
+};
+
 /* Services */
-
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+  value('version', '0.1'). // a simple value service
+  factory('session', function() {
+		// session: use localStorage to maintain session
+		// state across visits to the page and refreshes.
+
+		// TODO: 'today' should be injected, if it can.
+		var today = new Date();
+		var sessionKey = 'session';
+
+		var getExpirationDate = function (today) {
+			// expire in 12 hours. 
+			// 
+			// The purpose of the session is to let us 
+			// bounce around the site, and refresh the 
+			// page, without worrying too much. 
+			// 
+			// In other words, most of the data we're
+			// keeping track of in the session will 
+			// be irrelevant in 12 hours, anyway.
+			// 
+			var expirationDate = new Date();
+			expirationDate.setHours(today.getHours() + 12);
+			return expirationDate;
+		};
+
+		var defaultSession = function() {
+			return {
+				user: {
+					role: 'guest'
+				}
+			}
+		}(); // closure
+
+		var getSession = function() {
+			var session = store.get(sessionKey);
+			
+			if (!session) {
+				return session;
+			}
+			// Dates are stored as strings in JSON. That's cool,
+			// but we want to have actual Date objects.
+			if (session.expirationDate) {
+				session.expirationDate = new Date(session.expirationDate);
+			}
+
+			return session;
+		};
+
+		var session = getSession();
+		if (!session 
+		 || !session.expirationDate
+		 || session.expirationDate < today) {
+			// Load the default session if we don't have
+			// one in the local store, or if the one
+			// in the local store is stale.
+			session = defaultSession;
+		}
+		else {
+			// There is a non-stale session in local storage,
+			// so let's refresh the expiration date.
+			// TODO: Is this dumb? Should there be a 
+			// set time where we always clear out the
+			// session data?
+			session.expirationDate = getExpirationDate(today);
+		}
+
+		// Add our functions back, since JSON.stringify stripped them out.
+		if (!session.save) {
+			session.save = function() {
+				var now = new Date();
+				session.expirationDate = getExpirationDate(now);
+				store.set(sessionKey, session);
+			};
+		}
+
+		return session;
+	});
