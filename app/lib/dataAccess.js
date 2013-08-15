68f8f5ee5d82a4311f4a25cf2b270d7f72baf233
Phil <github@exclsr.com>
2013-08-14 19:02:36 -0700

    Refactor: Clean up dataAccess.js
    Move (most) function definitions out of the return
    statement, clean up the callbacks with if-else
    all around.

--- app/lib/dataAccess.js
+++ app/lib/dataAccess.js
@@ -6,137 +6,140 @@ var db = function() {
 	var isValidUser = function(user) {
 		return user && user.email && user.id;
 	}
+
+	var findUserByEmail = function(userEmail, callback) {
+		couch.users.findByEmail(userEmail, callback);
+	};
+
+	var findUserById = function(id, callback) {
+		couch.users.findById(id, callback);
+	};
+
+	var addUser = function(user, password, success, failure) {
+		if (!isValidUser(user)) {
+			return failure("User cannot be null?");
+		}
+
+		var addUser = function (user, password) {
+			couch.users.add(user, password, function (err, body) {
+				if (err) {
+					return failure(err);
+				}
+				success();	
+			});
+		};
+
+		findUserById(user.id, function (err, body) {
+			// TODO: Handle db errors.
+			if (body) {
+				// TODO: Error codes, etc.
+				// This is an internal error.
+				return failure("User already exists.");
+			}
+			findUserByEmail(user.email, function (err, body) {
+				if (body) {
+					//TODO: Error?
+					// This is an external error.
+					return failure("User email already exists");
+				}
+				addUser(user, password);
+			});
+		});
+	};
 	
+	var removeUser = function(user, success, failure) {
+		// 	if (!(user && user.email)) {
+		// 		// Do nothing. TODO: Do what?
+		// 	}
+		// 	else if (this.findById(user.id)) {
+		// 		// TODO: DO THIS ...
+		// 		return success();
+		// 	}
+
+		// 	// TODO: Error codes.
+		// 	return failure();
+	};
+
+	var updateUser = function(user, success, failure) {
+		if (!isValidUser(user)) {
+			return failure();
+		}
+
+		findUserById(user.id, function (err, user) {
+			if (err) {
+				return failure(err);
+			}
+			couch.users.update(user, function (err) {
+				if (err) {
+					return failure(err);
+				}
+				success();
+			});
+		});
+	};
+
+	var updateUserPassword = function(user, password, success, failure) {
+		// 	if (!isValidUser(user) && !this.findById(user.id)) {
+		// 		return failure();
+		// 	}
+
+		// 	// TODO ...
+		// 	// db.users.findById(user.id, function (err, user) {
+		// 	// 	if (err || !user) {
+		// 	// 		return failure();
+		// 	// 	}
+
+		// 	// 	db.passwords.findById(user.id, function (pass) {
+
+		// 	// 	});
+		// 	// });
+
+		// 	// var salt = uuid.v4();
+		// 	// var hash = hashPassword(password, salt);
+		// 	// passwordList[user.id] = { "hash":hash, "salt":salt };
+
+		// 	return success();
+	};
+
+	var validateUserPassword = function(user, password, success, failure) {
+		if(!isValidUser(user)) {
+			return failure();
+		}
+
+		couch.passwords.findById(user.id, function (err, pass) {
+			if (err || !pass) {
+				return failure();
+			}
+
+			var salt = pass.salt;
+			var hash = pass.hash;	
+			if(encrypt.hash(password, salt) === hash) {
+				return success();
+			}
+			return failure();
+		});
+	};
+
 	return {
 		users: { 
-			add: function(user, password, success, failure) {
-
-				var addUser = function (user, password) {
-					couch.users.add(user, password, function (err, body) {
-						if (err) {
-							failure(err);
-						}
-						else {
-							success();	
-						}
-					});
-				};
-
-				if (!isValidUser(user)) {
-					// TODO: Error?
-					console.log(user);
-					return failure("User cannot be null?");
-				}
-
-				var that = this;
-				this.findById(user.id, function (err, body) {
-					if (body) {
-						// TODO: Error codes, etc.
-						failure("User already exists.");
-					}
-					else {
-						that.findByEmail(user.email, function (err, body) {
-							if (body) {
-								//TODO: Error?
-								failure("User email already exists");
-							}
-							else {
-								addUser(user, password);
-							}
-						});
-					}
-				});
-			},
-			// remove: function(user, success, failure) {
-			// 	if (!(user && user.email)) {
-			// 		// Do nothing. TODO: Do what?
-			// 	}
-			// 	else if (this.findById(user.id)) {
-			// 		// TODO: DO THIS ...
-			// 		return success();
-			// 	}
-
-			// 	// TODO: Error codes.
-			// 	return failure();
-			// },
-			findByEmail: function(userEmail, callback) {
-				couch.users.findByEmail(userEmail, callback);
-			},
-			findById: function(id, callback) {
-				couch.users.findById(id, callback);
-			},
-			update: function(user, success, failure) {
-				if (!isValidUser(user)) {
-					return failure();
-				}
-
-				this.findById(user.id, function (err, user) {
-					if (err) {
-						return failure(err);
-					}
-					couch.users.update(user, function (err) {
-						if (err) {
-							return failure(err);
-						}
-						success();
-					});
-				});
-			},
-			// updatePassword: function(user, password, success, failure) {
-			// 	if (!isValidUser(user) && !this.findById(user.id)) {
-			// 		return failure();
-			// 	}
-
-			// 	// TODO ...
-			// 	// db.users.findById(user.id, function (err, user) {
-			// 	// 	if (err || !user) {
-			// 	// 		return failure();
-			// 	// 	}
-
-			// 	// 	db.passwords.findById(user.id, function (pass) {
-
-			// 	// 	});
-			// 	// });
-
-			// 	// var salt = uuid.v4();
-			// 	// var hash = hashPassword(password, salt);
-			// 	// passwordList[user.id] = { "hash":hash, "salt":salt };
-
-			// 	return success();
-			// },
-			validatePassword: function(user, password, success, failure) {
-				if(!isValidUser(user)) {
-					return failure();
-				}
-
-				couch.passwords.findById(user.id, function (err, pass) {
-					if (err || !pass) {
-						return failure();
-					}
-
-					var salt = pass.salt;
-					var hash = pass.hash;	
-					if(encrypt.hash(password, salt) === hash) {
-						return success();
-					}
-					return failure();
-				});
-			},
+			add: addUser,
+			// remove: removeUser,
+			findByEmail: findUserByEmail,
+			findById: findUserById, 
+			update: updateUser,
+			// updatePassword: updateUserPassword,
+			validatePassword: validateUserPassword,
 			getAll: function(callback) {
 				couch.users.getAll(callback);
 			},
 			count: function(callback) {
 				couch.users.getAll(function (err, users) {
-					if (err) {
-						callback(err);
-					}
-					else {
-						callback(null, users.length);
-					}
+					var userCount = users ? users.length : null;
+					callback(err, userCount);
 				});
 			}
 		}
-	}; // end return
+	};
 }();
 
 
