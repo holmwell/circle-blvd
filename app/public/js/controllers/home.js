c8fa0bb507d9cd08cc252853ea399b3b6e9ee520
Phil Manijak <github@exclsr.com>
2014-02-03 17:26:34 -0800

    WIP: Backlog

--- app/public/js/controllers/home.js
+++ app/public/js/controllers/home.js
@@ -1,4 +1,19 @@
 function HomeCtrl($scope) {
 
+	var stories = [];
+	stories[0] = {
+		id: 1,
+		summary: "What whaaaat"
+	};
+	stories[1] = {
+		id: 2,
+		summary: "Wooorddd"
+	};
+	stories[2] = {
+		id: 3,
+		summary: "Ok!"
+	}
+
+	$scope.stories = stories;
 }
 HomeCtrl.$inject = ['$scope'];
