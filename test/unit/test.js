39247d9945bda2dded6e3edcc101b986df3c3c39
Phil Manijak <github@exclsr.com>
2014-03-25 19:03:33 -0700

    Test: HomeCtrl: Use $httpBackend instead of $http
    It's important to realize that Angular injects things
    into controller constructors for you, and that you
    can break things if you try to do it yourself, like
    with $httpBackend.

--- test/unit/test.js
+++ test/unit/test.js
@@ -4,24 +4,53 @@ describe('HomeCtrl', function(){
 	var ctrl;
 
 	// angular services
-	var scope;
-	var doc;
-	var timeout;
-	var http;
+	var $scope;
+	// var $document;
+	// var $timeout;
+	// var $httpBackend;
 
-	beforeEach(inject(function ($injector, $rootScope, $controller) {
-		scope = $rootScope.$new();
-		timeout = $injector.get('$timeout');
-		doc = $injector.get('$document');
-		http = $injector.get('$http'); // TODO: httpBackend
+	beforeEach(inject(function ($injector) {
+		$scope = $injector.get('$rootScope').$new();
+
+		// var $timeout = $injector.get('$timeout');
+		// var $document = $injector.get('$document');
+		var $httpBackend = $injector.get('$httpBackend'); // TODO: httpBackend
+
+		$httpBackend.when('GET', '/data/1/first-story')
+		.respond({
+			"id": "2",
+			"summary": "two",
+			"projectId": "1",
+			"nextId": "3",
+			"isFirstStory": true
+		});
+
+		$httpBackend.when('GET', '/data/1/stories')
+		.respond({
+  			"2": {
+    			"id": "2",
+    			"summary": "two",
+    			"projectId": "1",
+    			"nextId": "3",
+    			"isFirstStory": true
+  			},
+  			"3": {
+    			"id": "3",
+    			"summary": "three",
+    			"projectId": "1",
+    			"isFirstStory": false
+  			}
+		});
 
 		var params = {
-			$scope: scope,
-			$timeout: timeout,
-			'$document': doc,
-			$http: http
+			'$scope': $scope,
+			// Angular takes care of injecting these:
+			// '$timeout': $timeout,
+			// '$document': $document,
+			// '$http': $httpBackend
 		};
 
+		var $controller = $injector.get('$controller');
 		ctrl = $controller(HomeCtrl, params);
 	}));
 
@@ -30,5 +59,12 @@ describe('HomeCtrl', function(){
 		expect(ctrl).not.toBe(null);
 	});
 
+	// it('whatever', function () {
+	// 	// console.log('ok!')
+	// 	// var s = $scope.stories;
+	// 	// for (var story in s) {
+	// 	// 	console.log(story);
+	// 	// }
+	// });
 
 });
