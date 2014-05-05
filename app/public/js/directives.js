390057f836a3b6499c2088efb2959e17d02dd625
Phil Manijak <github@exclsr.com>
2014-05-04 23:30:05 -0700

    Backlog: Parse comments for links
    It's pretty much expected that URLs will
    be auto-linked by any modern app, so there
    it is -- in the comments anyway.

--- app/public/js/directives.js
+++ app/public/js/directives.js
@@ -8,4 +8,39 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }]).
+  directive('parseUrl', function() {
+  	  // Source: http://jsfiddle.net/bmleite/FyGen/
+  	  // Reference: http://stackoverflow.com/questions/14692640/angularjs-directive-to-replace-text
+	  var urlPattern = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/gi;
+	    
+	  var normalize = function (arr) {
+	  	if (!arr) {
+	  		return "";
+	  	}
+
+	      var result = {};
+	       for (var i = 0, l = arr.length; i < l; i++) {
+	          if (!result.hasOwnProperty(arr[i])) {
+	             result[arr[i]] = arr[i];
+	          }
+	       }
+	       return result;
+	  };
+	  
+	  return {    
+	    restrict: 'A',    
+	    require: 'ngModel',
+	    replace: true,   
+	    // scope: { props: '=parseurl', ngModel: '=ngModel' },
+	    scope: { ngModel: '=ngModel' },
+	    link: function compile(scope, element, attrs, controller) {
+	        scope.$watch('ngModel', function(value) {                     
+	            angular.forEach(normalize(value.match(urlPattern)), function(url) {                                    
+	                value = value.replace(new RegExp(url, 'g'), '<a href="'+ url + '">' + url +'</a>');     
+	            });
+	            element.html(value);
+	          });                
+	    }
+	  };  
+});
