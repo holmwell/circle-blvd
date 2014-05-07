ef4a883cc8a727694a9755671d7df3d7d5b15eed
Phil Manijak <github@exclsr.com>
2014-05-07 11:46:18 -0700

    Backlog: Linky comments
    Use Angular's linky filter to allow links in comments
    without allowing scripting attacks.
    
    Throw the regex hack from the last commit in the trash.

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -4,14 +4,15 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
 	'ngRoute',
+	'ngSanitize',
 	'myApp.filters', 
 	'myApp.services', 
 	'myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
-  	$routeProvider.when('/', {templateUrl: 'partials/home.html', controller: HomeCtrl});
-    $routeProvider.when('/signin', {templateUrl: 'partials/signin.html', controller: SignInCtrl});
-    $routeProvider.when('/profile', {templateUrl: 'partials/profile.html', controller: ProfileCtrl});
-    $routeProvider.when('/admin', {templateUrl: 'partials/admin.html', controller: AdminCtrl});
-    $routeProvider.when('/initialize', {templateUrl: 'partials/initialize.html', controller: InitializeCtrl});
-    $routeProvider.otherwise({redirectTo: '/'});
-  }]);
+	config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/', {templateUrl: 'partials/home.html', controller: HomeCtrl});
+		$routeProvider.when('/signin', {templateUrl: 'partials/signin.html', controller: SignInCtrl});
+		$routeProvider.when('/profile', {templateUrl: 'partials/profile.html', controller: ProfileCtrl});
+		$routeProvider.when('/admin', {templateUrl: 'partials/admin.html', controller: AdminCtrl});
+		$routeProvider.when('/initialize', {templateUrl: 'partials/initialize.html', controller: InitializeCtrl});
+		$routeProvider.otherwise({redirectTo: '/'});
+	}]);
--- app/public/js/directives.js
+++ app/public/js/directives.js
@@ -19,48 +19,23 @@ function escapeHtml(string) {
 
 angular.module('myApp.directives', []).
   directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
+	return function(scope, elm, attrs) {
+	  elm.text(version);
+	};
   }]).
-  directive('parseUrlHack', function() {
-  	  // Source: http://jsfiddle.net/bmleite/FyGen/
-  	  // Reference: http://stackoverflow.com/questions/14692640/angularjs-directive-to-replace-text
-	  var urlPattern = /(http|ftp|https):&#x2F;&#x2F;[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/gi;
-	    
-	  var normalize = function (arr) {
-	  	if (!arr) {
-	  		return "";
-	  	}
-
-	      var result = {};
-	       for (var i = 0, l = arr.length; i < l; i++) {
-	          if (!result.hasOwnProperty(arr[i])) {
-	             result[arr[i]] = arr[i];
-	          }
-	       }
-	       return result;
-	  };
-
-	  return {    
-	    restrict: 'A',    
-	    require: 'ngModel',
-	    replace: true,   
-	    // scope: { props: '=parseurl', ngModel: '=ngModel' },
-	    scope: { ngModel: '=ngModel' },
-	    link: function compile(scope, element, attrs, controller) {
-	        scope.$watch('ngModel', function(value) {
-	        	value = escapeHtml(value);
-	            angular.forEach(normalize(value.match(urlPattern)), function(url) {
-	                value = value.replace(new RegExp(url, 'g'), '<a href="'+ url + '">' + url +'</a>');
-	            });
-	            // This is a hack to prepend whatever is in this block
-	            // at the start. (e.g. the name in a comment)
-	            element.html(element.html() + value);
-	          });                
-	    }
-	  };  
-}).
+directive('appendLinky', ['$filter', function ($filter) {
+	return {
+		restrict: 'A',
+		replace: true,
+		scope: { ngModel: '=ngModel' },
+		link: function (scope, element, attrs, controller) {
+			scope.$watch('ngModel', function (value) {
+			 	value = $filter('linky')(value);
+			 	element.html(element.html() + value);
+			});
+		}
+	};
+}]).
 directive('autosize', function () {
 	return {
 		restrict: 'A',
