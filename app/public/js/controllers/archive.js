7f4da05abe6ef5d40c457aa7153f448a2eb184b8
Phil Manijak <github@exclsr.com>
2014-05-09 12:45:49 -0700

    Archives: Show details when selected
    A rough sketch for showing details of
    an archive.

--- app/public/js/controllers/archive.js
+++ app/public/js/controllers/archive.js
@@ -1,5 +1,6 @@
 function ArchivesCtrl($scope, $http) {
 	var projectId = "1";
+	var selectedArchive = undefined;
 
 	$http.get('/data/' + projectId + '/archives')
 	.success(function (data) {
@@ -9,5 +10,35 @@ function ArchivesCtrl($scope, $http) {
 		console.log("Failed to get archives.");
 		console.log(data);
 	});
+
+	$scope.select = function (archive) {
+		if (archive.justDeselected) {
+			// HACK: So right now whenever we call deselect,
+			// the click event also bubbles up (or whatever)
+			// to this method.
+			archive.justDeselected = undefined;
+			return;
+		}
+
+		// Do not refocus stuff if we're already on this archive.
+		if (!archive.isSelected) {
+			// Hide the previously-selected archive
+			if (selectedArchive) {
+				selectedArchive.isSelected = false;
+			}
+
+			archive.isSelected = true;
+			selectedArchive = archive;
+		}	
+	};
+
+	$scope.deselect = function (archive) {
+		if (archive && archive.isSelected) {
+			archive.isSelected = false;
+			archive.justDeselected = true;
+			
+			selectedArchive = undefined;
+		}
+	};
 }
 ArchivesCtrl.$inject = ['$scope', '$http'];
