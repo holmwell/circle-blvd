b7db00ec9347cfe338a72a0e47baf7fbb45e2a21
Phil Manijak <github@exclsr.com>
2014-07-06 14:11:35 -0700

    Refactor: Settings: Move init into lib

---
+++ app/lib/settings.js
@@ -0,0 +1,118 @@
+// settings.js
+var db = {};
+db.settings = require('./data-settings.js');
+
+module.exports = function () {
+	
+	var init = function (callback) {
+		// Visibility definitions:
+		//   public: visible to all
+		//   private: visible to administrators
+		//   secret: visible to the database and computer memory
+		var defaultSettings = [{
+			name: "demo",
+			value: false,
+			visibility: "public"
+		},{
+			name: 'session-secret',
+			value: uuid.v4(),
+			visibility: "secret"
+		},{
+			name: "smtp-login",
+			value: null,
+			visibility: "private"
+		},{
+			name: "smtp-password",
+			value: null,
+			visibility: "secret"
+		},{
+			name: "smtp-service",
+			value: "Zoho",
+			visibility: "private"
+		},{ 
+			name: "ssl-ca-path",
+			value: null,
+			visibility: "private"
+		},{
+			name: "ssl-cert-path",
+			value: null,
+			visibility: "private"
+		},{
+			name: "ssl-key-path",
+			value: null,
+			visibility: "private"
+		},{
+			name: "stripe-public-key",
+			value: null,
+			visibility: "public"
+		},{
+			name: "stripe-secret-key",
+			value: null,
+			visibility: "secret"
+		}];
+
+		var settingsTable = {};
+		var initialized = {};
+		defaultSettings.forEach(function (setting) {
+			settingsTable[setting.name] = setting;
+			initialized[setting.name] = undefined;
+		});
+
+		var callbackIfAllSettingsReady = function () {
+			var isReady = true;
+			for (var key in initialized) {
+				if (initialized[key] === undefined) {
+					isReady = false;
+					break;
+				}
+			}
+
+			if (isReady && callback) {
+				callback(null, initialized);
+			}
+		};
+
+		db.settings.getAll(function (savedSettings) {
+			// var savedSetting;
+			// var defaultSetting;
+			var settingFound;
+			var settingReady = function (setting) {
+				initialized[setting.name] = setting;
+				callbackIfAllSettingsReady();
+			};
+
+			for (var defaultName in settingsTable) {
+				
+				settingFound = false;
+				for (var savedName in savedSettings) {
+					if (savedName === defaultName) {
+						settingFound = true;
+						break;
+					}
+				}
+
+				if (!settingFound) {
+					var addSettingToDatabase = function (settingToAdd) {
+						db.settings.add(settingToAdd, 
+							function (body) {
+								settingReady(settingToAdd);
+							},
+							function (err) {
+								callback({
+									message: "Could not set setting: " + settingToAdd.name
+								});
+							}
+						);
+					}(settingsTable[defaultName]);				
+				}
+				else {
+					settingReady(savedSettings[savedName]);
+				}
+			}
+		});
+	};
+
+	return {
+		init: init
+	};
+}();
--- app/server.js
+++ app/server.js
@@ -14,6 +14,7 @@ var db       = require('./lib/dataAccess.js').instance();
 
 var sslServer = require('./lib/https-server.js');
 var payment   = require('./lib/payment.js');
+var settings  = require('./lib/settings.js');
 
 var usersRoutes = require('./routes/users');
 var userRoutes 	= require('./routes/user');
@@ -1456,114 +1457,6 @@ var forceHttps = function(req, res, next) {
 	res.redirect('https://' + req.get('Host') + req.url);
 };
 
-var initSettings = function (callback) {
-	// Visibility definitions:
-	//   public: visible to all
-	//   private: visible to administrators
-	//   secret: visible to the database and computer memory
-	var defaultSettings = [{
-		name: "demo",
-		value: false,
-		visibility: "public"
-	},{
-		name: 'session-secret',
-		value: uuid.v4(),
-		visibility: "secret"
-	},{
-		name: "smtp-login",
-		value: null,
-		visibility: "private"
-	},{
-		name: "smtp-password",
-		value: null,
-		visibility: "secret"
-	},{
-		name: "smtp-service",
-		value: "Zoho",
-		visibility: "private"
-	},{ 
-		name: "ssl-ca-path",
-		value: null,
-		visibility: "private"
-	},{
-		name: "ssl-cert-path",
-		value: null,
-		visibility: "private"
-	},{
-		name: "ssl-key-path",
-		value: null,
-		visibility: "private"
-	},{
-		name: "stripe-public-key",
-		value: null,
-		visibility: "public"
-	},{
-		name: "stripe-secret-key",
-		value: null,
-		visibility: "secret"
-	}];
-
-	var settingsTable = {};
-	var initialized = {};
-	defaultSettings.forEach(function (setting) {
-		settingsTable[setting.name] = setting;
-		initialized[setting.name] = undefined;
-	});
-
-	var callbackIfAllSettingsReady = function () {
-		var isReady = true;
-		for (var key in initialized) {
-			if (initialized[key] === undefined) {
-				isReady = false;
-				break;
-			}
-		}
-
-		if (isReady && callback) {
-			callback(null, initialized);
-		}
-	};
-
-	db.settings.getAll(function (savedSettings) {
-		// var savedSetting;
-		// var defaultSetting;
-		var settingFound;
-		var settingReady = function (setting) {
-			initialized[setting.name] = setting;
-			callbackIfAllSettingsReady();
-		};
-
-		for (var defaultName in settingsTable) {
-			
-			settingFound = false;
-			for (var savedName in savedSettings) {
-				if (savedName === defaultName) {
-					settingFound = true;
-					break;
-				}
-			}
-
-			if (!settingFound) {
-				var addSettingToDatabase = function (settingToAdd) {
-					db.settings.add(settingToAdd, 
-						function (body) {
-							settingReady(settingToAdd);
-						},
-						function (err) {
-							callback({
-								message: "Could not set setting: " + settingToAdd.name
-							});
-						}
-					);
-				}(settingsTable[defaultName]);				
-			}
-			else {
-				settingReady(savedSettings[savedName]);
-			}
-		}
-	});
-};
-
 var getCookieSettings = function () {
 	// TODO: Check settings to guess if https is running.
 	// Or actually figure out if https is running, and if so
@@ -1617,7 +1510,7 @@ app.configure(function() {
 
 	var tenSeconds = 10000;
 	db.whenReady(function () {
-		initSettings(function (err, settings) {
+		settings.init(function (err, settings) {
 			if (err) {
 				console.log(err);
 			}
