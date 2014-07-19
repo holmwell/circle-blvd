7e83c9e7542804397a24f71d4aa3605e735f2868
Phil Manijak <github@exclsr.com>
2014-07-18 22:00:15 -0700

    Refactor: Settings: Remove non-standard callbacks
    With the removal of the demo setting, that means
    most of data-settings.js simply points to
    couch-settings.js

--- app/lib/data-settings.js
+++ app/lib/data-settings.js
@@ -12,33 +12,12 @@ module.exports = function () {
 		couch.settings.add(newSetting, callback);
 	};
 
-	var saveSetting = function(setting, success, failure) {
-		couch.settings.update(setting, function (err, newSetting) {
-			if (err) {
-				return failure(err);
-			}
-			return success(newSetting);
-		});
-	};
-
-	// TODO: Refactor, get rid of the save setting stuff.
-	var updateSetting = function (setting, callback) {
-		saveSetting(setting, 
-			function success (newSetting) {
-				callback(null, newSetting);
-			},
-			function failure (err) {
-				callback(err);
-			}
-		);
-	};
-
 	return {
 		add: addSetting,
 		get: couch.settings.get,
 		getAuthorized: couch.settings.getAuthorized,
 		getPrivate: couch.settings.getPrivate,
 		getAll: couch.settings.getAll,
-		update: updateSetting
+		update: couch.settings.update
 	};
 }(); // closure
