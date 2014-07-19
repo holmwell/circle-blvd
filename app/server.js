e171bf49f8569bdcedf8642657dad9aa1dcd3083
Phil Manijak <github@exclsr.com>
2014-07-19 02:10:42 -0700

    Refactor: Move circle creation into data layer
    Move createCircle out of server.js and into
    dataAccess.js. It will need to move again, but
    at least it is out of server.js.

--- app/server.js
+++ app/server.js
@@ -331,181 +331,6 @@ var configureSuccessful = function () {
 
 	app.get("/data/circles/all", ensure.mainframe, send(db.circles.getAll));
 
-	var addStoriesForNewCircle = function (newCircle, adminAccount, callback) {
-		
-		var newStory = function () {
-			var story = {};
-			story.projectId = newCircle._id;
-			return story;
-		};
-
-		var welcome = newStory();
-		welcome.summary = "Welcome to Circle Blvd.";
-		welcome.status = "active";
-		welcome.isFirstStory = true;
-		welcome.description = "Hi! This is a story list. The main idea is that " +
-		"stories closer to the top want to be completed before the ones closer " + 
-		"to the bottom.\n\nPlay around with it. Maybe start by moving the 'Next " + 
-		"meeting' around.";
-
-		var addStories = newStory();
-		addStories.summary = "To get started, add a few stories";
-		addStories.owner = adminAccount.name;
-		addStories.description = "Please see the 'Add story' link, at the top " +
-		"of the story list, to get started.";
-
-		var addTeamMembers = newStory();
-		addTeamMembers.summary = "When you're ready, add some team members";
-		addTeamMembers.status = "assigned";
-		addTeamMembers.description = "You can do this from the Admin page.";
-
-		var seeDocs = newStory();
-		seeDocs.summary = "Check out the documentation for more details";
-
-		var readyMilepost = newStory();
-		readyMilepost.isDeadline = true;
-		readyMilepost.summary = "Start using the site";
-
-		var nextMeeting = newStory();
-		nextMeeting.summary = "Next meeting";
-		nextMeeting.isNextMeeting = true;
-
-		var subscribe = newStory();
-		subscribe.summary = "Subscribe, if you can";
-		subscribe.owner = adminAccount.name;
-		subscribe.description = "Circle Blvd. may be used for free, for a reasonable " +
-		"amount of time. Like Wikipedia, we rely on donations to keep the site online. You " +
-		"can subscribe from the profile page, and end your subscription at any time.";
-
-		var haveFun = newStory();
-		haveFun.summary = "Have fun :-)";
-		haveFun.status = "assigned";
-		haveFun.description = "Please enjoy using our site. To send us a note, you can find " + 
-		"us on Twitter at @circleblvd. Thank you!";
-
-		var stories = [
-			welcome,
-			addStories,
-			addTeamMembers,
-			seeDocs,
-			readyMilepost,
-			nextMeeting,
-			subscribe,
-			haveFun
-		];
-		stories.reverse();
-
-		var currentIndex = 0;
-
-		var addStory = function (story, nextId) {
-			if (nextId) {
-				story.nextId = nextId;
-			}
-			db.stories.add(story, function (err, body) {
-				if (err) {
-					return callback(err);
-				}
-				currentIndex++;
-				if (currentIndex >= stories.length) {
-					callback();
-				}
-				else {
-					addStory(stories[currentIndex], body.id);
-				}
-			});	
-		};
-
-		addStory(stories[currentIndex]);
-	};
-
-	var createCircle = function (circleName, adminEmailAddress, callback) {
-		var circle = {
-			name: circleName
-		};
-
-		db.users.findByEmail(adminEmailAddress, function (err, adminAccount) {
-			if (err) {
-				return callback(err);
-			}
-
-			circle.createdBy = {
-				name: adminAccount.name,
-				id: adminAccount._id
-			};
-
-			db.circles.add(circle, function (err, newCircle) {
-				if (err) {
-					return callback(err);
-				}
-
-				var administrativeGroup = {
-					name: "Administrative",
-					projectId: newCircle._id,
-					isPermanent: true
-				};
-
-				var impliedGroup = {
-					name: "_implied",
-					projectId: newCircle._id,
-					isPermanent: true
-				};
-
-				addStoriesForNewCircle(newCircle, adminAccount, function (err, body) {
-					if (err) {
-						return callback(err);
-					}
-
-					db.groups.add(administrativeGroup, function (adminGroup) {
-						db.groups.add(impliedGroup, function (memberGroup) {
-
-							var addCircleMembershipsToAdmin = function (account) {
-								// admin access
-								account.memberships.push({
-									circle: newCircle._id,
-									group: adminGroup.id,
-									level: "member"
-								});
-								// member access
-								account.memberships.push({
-									circle: newCircle._id,
-									group: memberGroup.id,
-									level: "member"
-								});
-
-								db.users.update(account, 
-									function (body) {
-										callback(null, newCircle);
-									},
-									function (err) {
-										callback(err);
-									}
-								);
-							};
-
-							if (adminAccount) {
-								addCircleMembershipsToAdmin(adminAccount);
-							}
-							else {
-								var err = {};
-								err.message = "Admin account was not found. Cannot create circle " +
-								"witout an exiting admin account.";
-								callback(err);
-							}
-						},
-						function (err) {
-							// failure adding member group
-							callback(err);
-						});
-					},
-					function (err) {
-						// failure adding admin group
-						callback(err);
-					});
-				});
-
-			})
-		});
-	};
 
 	var checkCircleLimit = function (res, next) {
 		checkLimit(next);
@@ -569,7 +394,7 @@ var configureSuccessful = function () {
 					return res.send(403, "Sorry, you can only create " + maxCircleCount + " circles.");
 				}
 
-				createCircle(circleName, admin.email, handle(res));
+				db.circles.create(circleName, admin.email, handle(res));
 			}));
 		}
 	});
@@ -582,7 +407,7 @@ var configureSuccessful = function () {
 			return res.send(400, "An email address for an administrative user is required when making a circle.");
 		}
 
-		createCircle(circle.name, admin.email, handle(res));
+		db.circles.create(circle.name, admin.email, handle(res));
 	});
 
 	app.put("/data/circle", ensure.mainframe, function (req, res) {
@@ -1124,7 +949,7 @@ var configureSuccessful = function () {
 			};
 
 			var userAccountCreated = function (newAccount) {
-				createCircle(proposedCircle.name, newAccount.email, handle(res));
+				db.circles.create(proposedCircle.name, newAccount.email, handle(res));
 			};
 
 			db.users.findByEmail(proposedAccount.email, guard(res, function (accountExists) {
