4be75bcb3bd7680d3668c927053ea7e68adc6a3d
Phil Manijak <github@exclsr.com>
2014-07-22 01:49:06 -0700

    Test: App: Add a new member
    Test the process of adding a new
    member to the first circle.

--- app/test/app.js
+++ app/test/app.js
@@ -6,8 +6,27 @@ var databaseName = 'a-tmp-db-for-circle-blvd-testing';
 var sessionsDatabaseName = databaseName + '-sessions';
 process.env.DATABASE_NAME = databaseName;
 
-var unit = undefined;
-var test = {};
+var unit  = undefined;
+var test  = {};
+
+var admin = undefined;
+var member = undefined;
+
+var adminEmail = 'admin@test';
+var adminPassword = 'Well, what do you think?';
+var adminSession = {};
+
+var memberEmail = "member@test";
+var memberPassword = 'Members only!';
+var memberSession = {};
+
+var finish = function (test) {
+	var fn = function (err) {
+		test.ifError(err);
+		test.done();
+	};
+	return fn;
+};
 
 test['database setup'] = function (test) {
 	var app = require('../app.js');
@@ -21,34 +40,143 @@ test['database setup'] = function (test) {
 			test.done();
 		}, halfSecond);	
 	});
+
+	// for a persistent session
+	admin = request.agent(unit);
+	member = request.agent(unit);
 };
 
-test['first call does a redirect'] = function (test) {
+test['GET / is 302'] = function (test) {
 	request(unit)
 	.get('/')
 	.expect(302)
-	.end(function (err) {
-		test.ifError(err);
-		test.done();
-	});
+	.end(finish(test));
 };
 
-test['/data/initialize succeeds'] = function (test) {
+test['POST /data/initialize is 200'] = function (test) {
 	var data = {};
 	data.admin = {
-		email: 'test@ok.com',
-		password: 'Well, what do you think?'
+		email: adminEmail,
+		password: adminPassword
 	};
 
 	request(unit)
 	.put('/data/initialize')
 	.send(data)
 	.expect(200)
-	.end(function (err) {
+	.end(finish(test));
+};
+
+test['GET /data/user is 401 at first'] = function (test) {
+	admin
+	.get('/data/user')
+	.expect(401)
+	.end(finish(test));
+};
+
+test['POST /auth/signin is 401 with bad password'] = function (test) {
+	admin
+	.post('/auth/signin')
+	.type("form")
+	.send({ email: adminEmail })
+	.send({ password: 'invalid password' })
+	.expect(401)
+	.end(finish(test));
+};
+
+test['POST /auth/signin is 401 with unknown id'] = function (test) {
+	admin
+	.post('/auth/signin')
+	.type("form")
+	.send({ email: 'unknown id' })
+	.send({ password: 'invalid password' })
+	.expect(401)
+	.end(finish(test));
+};
+
+test['POST /auth/signin is 200'] = function (test) {
+	admin
+	.post('/auth/signin')
+	.type("form")
+	.send({ email: adminEmail })
+	.send({ password: adminPassword })
+	.expect(200)
+	.end(finish(test));
+};
+
+test['GET /data/circles'] = function (test) {
+	admin
+	.get('/data/circles')
+	.expect(200)
+	.end(function (err, res) {
 		test.ifError(err);
+		for (var key in res.body) {
+			var circle = res.body[key];
+			// save circle id in session
+			if (!adminSession.circle) {
+				adminSession.circle = circle;
+			}
+		}
 		test.done();
 	});
 };
+
+test['GET /data/:circleId/groups is 200'] = function (test) {
+	admin
+	.get('/data/' + adminSession.circle._id + '/groups')
+	.expect(200)
+	.end(function (err, res) {
+		test.ifError(err);
+		for (var key in res.body) {
+			var group = res.body[key];
+			if (!adminSession.group) {
+				adminSession.group = group;
+			}
+		}
+		test.done();
+	})
+};
+
+test['POST /data/:circleId/member is 204'] = function (test) {
+	// TODO: This API is lame and needs to be redone.
+	var memberships = [];
+	memberships.push({
+		circle: adminSession.circle._id,
+		group: adminSession.group._id,
+		level: "member"
+	});
+
+	admin
+	.post('/data/' + adminSession.circle._id + '/member')
+	.send({
+		name: "Test member",
+		email: memberEmail,
+		password: memberPassword,
+		memberships: memberships,
+		isReadOnly: false
+	})
+	.expect(204)
+	.end(finish(test));
+};
+
+test['Sign in the non-admin member'] = function (test) {
+	member
+	.post('/auth/signin')
+	.type("form")
+	.send({ email: memberEmail })
+	.send({ password: memberPassword })
+	.expect(200)
+	.end(finish(test));
+};
+
+test['GET /data/user is 200 after signin'] = function (test) {
+	member
+	.get('/data/user')
+	.expect(200)
+	.end(finish(test));
+};
+
+
 
 test['database tear down'] = function (test) {
 	var destroyTestDatabase = function (callback) {
@@ -60,6 +188,7 @@ test['database tear down'] = function (test) {
 
 	var destroy = [destroyTestDatabase, destroyTestSessionsDb];
 	async.series(destroy, function (err, results) {
+		test.ifError(err);
 		test.done();
 	});
 };
