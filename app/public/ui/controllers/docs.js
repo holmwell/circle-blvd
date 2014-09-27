3d84658de0f914ac8ad1d613e0f66d0da03c7674
Phil Manijak <github@exclsr.com>
2014-09-27 16:50:27 -0700

    Docs: Add features since 0.7 and review
    Add some basic information about the major
    features of the site that we've added since
    0.7: labels, multi-story entry, invites,
    mindsets: bump and roadmap.

--- app/public/ui/controllers/docs.js
+++ app/public/ui/controllers/docs.js
@@ -13,7 +13,7 @@ function DocsCtrl(session, lib, $scope) {
 		"id": "1",
 		"nextId": "2",
 		"isFirstStory": true,
-		"summary": "This story is done.",
+		"summary": "#Intro: This story is done",
 		"description": "Hi! This is the first story in this story-list " +
 		"demonstration. Please stomp around and see what this thing can " +
 		"do.",
@@ -30,6 +30,9 @@ function DocsCtrl(session, lib, $scope) {
 			},
 			"timestamp": 1402388429872
 		  }
+		],
+		"labels": [
+			"Intro"
 		]
 	};
 
@@ -43,23 +46,29 @@ function DocsCtrl(session, lib, $scope) {
 		"id": "2",
 		"nextId": "2.5",
 		"owner": "Nicholas",
-		"summary": "This one is in progress",
+		"summary": "#Intro: This one is in progress",
 		"description": "They're called 'stories' because, well, we " +
 		"don't know why. However, 'tasks' sounds a bit boring, don't you " +
 		"think?",
 		"status": "active",
 		"createdBy": {
 		  "name": "Phil",
-		}
+		},
+		"labels": [
+			"Intro"
+		]
 	  },
 	  "2.5": {
 		"id": "2.5",
 		"nextId": "3",
-		"summary": "This is a milepost",
+		"summary": "#Intro: This is a milepost",
 		"status": "",
 		"description": "Mileposts can be useful if things need to be " + 
 		"done before or after a specific event.",
-		isDeadline: true
+		isDeadline: true,
+		"labels": [
+			"Intro"
+		]
 	  },
 	  "3": {
 		"id": "3",
@@ -71,16 +80,46 @@ function DocsCtrl(session, lib, $scope) {
 	  	"be archived by pressing the large circle. Try it!",
 		"createdBy": {
 		  "name": "Phil",
-		}
+		},
+		"labels": [
+		]
 	  },
 	  "3.25": {
 		"id": "3.25",
-		"nextId": "3.5",
-		"summary": "Next meeting",
+		"nextId": "3.4",
+		"summary": "#Intro: Next meeting",
 		"description": "What needs to be done before your next meeting? " +
 		"Move this special story to bring your team's focus to the top of " + 
 		"your story list.",
 		"isNextMeeting": true,
+		"labels": [
+			"Intro"
+		]
+	  },
+	  "3.4": {
+		"id": "3.4",
+		"nextId": "3.45",
+		"summary": "#Intro: Use the grippy cross, on the right, to move stories",
+		"description": "This works on desktops, laptops, iPads and iPhones, " +
+		"but not on Android things (yet).",
+		"status": "active",
+		"createdBy": {
+		  "name": "Phil",
+		},
+		"labels": [
+			"Intro"
+		]
+	  },
+	  "3.45": {
+		"id": "3.45",
+		"nextId": "3.5",
+		"summary": "#Intro: Use mileposts as waypoints in your journey",
+		"status": "",
+		"description": "They're the best.",
+		isDeadline: true,
+		"labels": [
+			"Intro"
+		]
 	  },
 	  "3.5": {
 	  	"id": "3.5",
@@ -90,58 +129,196 @@ function DocsCtrl(session, lib, $scope) {
 		"next to them.",
 	  	"createdBy": {
 		  "name": "Phil",
-		}
+		},
+		"labels": [
+		]
 	  },
 	  "4": {
 		"id": "4",
-		"nextId": "6",
+		"nextId": "7",
 		"summary": "There's an issue with this story",
 		"description": "Of course, sometimes stories need to be talked about.",
 		"status": "sad",
 		"createdBy": {
 		  "name": "Phil",
-		}
-	  },
-	  "6": {
-		"id": "6",
-		"nextId": "7",
-		"summary": "Use the grippy cross, on the right, to move stories",
-		"description": "This works on desktops, laptops, iPads and iPhones, " +
-		"but not on Android things (yet).",
-		"owner": me,
-		"status": "",
-		"createdBy": {
-		  "name": "Phil",
-		}
+		},
+		"labels": [
+		]
 	  },
 	  "7": {
 		"id": "7",
-		"nextId": "8",
+		"nextId": "7.1",
 		"summary": "Click stories to see details",
 		"description": "Thanks. :-)",
 		"createdBy": {
 		  "name": "Phil",
-		}
+		},
+		"labels": [
+		]
+	  },
+	  "7.1": {
+		"id": "7.1",
+		"nextId": "7.2",
+		"summary": "Click circles to send done stories to the archives",
+		"description": "Maybe mark a story as 'done' when a task is complete, " +
+			"and then archive it when you're really done thinking about it.",
+		"status": "done",
+		"createdBy": {
+		  "name": "Phil",
+		},
+		"labels": [
+		]
+	  },
+	  "7.2": {
+		"id": "7.2",
+		"nextId": "7.3",
+		"summary": "Commenting on stories will email the story creator ...",
+		"description": "... and it will email the story owner (the person assigned), " +
+			" and whoever else has commented on this particular story.",
+		"createdBy": {
+		  "name": "Phil",
+		},
+		"labels": [
+		]
+	  },
+	  "7.3": {
+		"id": "7.3",
+		"nextId": "7.4",
+		"summary": "Click the envelope icon to email the story owner",
+		"owner": "Nicholas",
+		"description": "You can change the email address you receive notifications " +
+			" on the profile page.",
+		"createdBy": {
+		  "name": "Phil",
+		},
+		"labels": [
+		]
+	  },
+	  "7.4": {
+		"id": "7.4",
+		"nextId": "8",
+		"summary": "#Tag stories with hashtags, and click to filter",
+		"owner": "Nicholas",
+		"description": "",
+		"createdBy": {
+		  "name": "Phil",
+		},
+		"labels": [
+			"Tag", "#hashtags"
+		]
 	  },
 	  "8": {
 		"id": "8",
-		"nextId": "9",
-		"summary": "Use mileposts as waypoints in your journey",
+		"nextId": "8.1",
+		"summary": "Use the entry panel",
 		"status": "",
-		"description": "They're the best.",
-		isDeadline: true
+		isDeadline: true,
+		"createdBy": {
+		  "name": "Phil",
+		},
+		"labels": [
+		]
 	  },
+	  "8.1": {
+		"id": "8.1",
+		"nextId": "8.2",
+		"summary": "#Entry: Add many stories at once with the 'many' option",
+		"status": "",
+		"description": "Specify one story per line\n" +
+			"-- Specify mileposts by starting with two dashes\n" +
+			"Assign stories by ending them like @Nicholas",
+		"createdBy": {
+		  "name": "Phil",
+		},
+		"labels": [
+			"Entry"
+		]
+	  },
+	  "8.2": {
+		"id": "8.2",
+		"nextId": "9",
+		"summary": "#Entry: Use checklists for recurring tasks",
+		"status": "",
+		"description": "You can save lists of stories on the checklists page. " +
+			"Maybe you have a checklist for publishing your work?",
+		"createdBy": {
+		  "name": "Phil",
+		},
+		"labels": [
+			"Entry"
+		]
+	  },
+
 	  "9": {
 		"id": "9",
-		"nextId": "10",
-		"summary": "Have fun :-)",
+		"nextId": "9.1",
+		"summary": "Change mindsets with the top-right icon panel",
+		"status": "",
+		isDeadline: true,
+		"createdBy": {
+		  "name": "Phil",
+		},
+		"labels": [
+		]
+	  },
+	  "9.1": {
+		"id": "9.1",
+		"nextId": "9.2",
+		"summary": "The caret icon ^ is for bumping stories to the top of the list ^",
 		"status": "",
 		"createdBy": {
 		  "name": "Phil",
-		}
+		},
+		"labels": [
+		]
 	  },
-	  "10": {
-		"id": "10",
+	  "9.2": {
+		"id": "9.2",
+		"nextId": "10.0",
+		"summary": "The road icon / I \\ is for moving stories great distances / I \\",
+		"status": "",
+		"createdBy": {
+		  "name": "Phil",
+		},
+		"labels": [
+		]
+	  },
+	  "10.0": {
+		"id": "10.0",
+		"nextId": "10.1",
+		"summary": "Create up to four circles from your profile page",
+		"status": "",
+		"createdBy": {
+		  "name": "Phil",
+		},
+		"labels": [
+		]
+	  },
+	  "10.1": {
+		"id": "10.1",
+		"nextId": "10.10",
+		"summary": "Invite people to join your circle from the Admin page",
+		"status": "",
+		"createdBy": {
+		  "name": "Phil",
+		},
+		"labels": [
+		]
+	  },
+	  "10.10": {
+		"id": "10.10",
+		"nextId": "last",
+		"summary": "Have fun :-)",
+		"status": "",
+		isDeadline: true,
+		"createdBy": {
+		  "name": "Phil",
+		},
+		"labels": [
+		]
+	  },
+	  "last": {
+		"id": "last",
 		"nextId": "last-demo",
 		"summary": "That's it.",
 		"createdBy": {
