05e95dc40e876aa49228d7ef707d80446a093aa8
Phil Manijak <github@exclsr.com>
2015-07-20 00:53:42 -0700

    Tour: Add String.endsWith polyfill
    endsWith is in ES6 but is not yet supported
    by Safari and some other people.

--- web/public/ui/controllers/tour.js
+++ web/public/ui/controllers/tour.js
@@ -11,6 +11,19 @@ function TourCtrl(lib, $scope, $window) {
 	var sectionToShow = 'tags';
 	var workSectionToShow = 'details';
 
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+	if (!String.prototype.endsWith) {
+		String.prototype.endsWith = function(searchString, position) {
+			var subjectString = this.toString();
+			if (position === undefined || position > subjectString.length) {
+				position = subjectString.length;
+			}
+			position -= searchString.length;
+			var lastIndex = subjectString.indexOf(searchString, position);
+			return lastIndex !== -1 && lastIndex === position;
+		};
+	}
+
 	var pathname = $window.location.pathname;
 	if (pathname.indexOf('/tour/start') === 0 
 	|| pathname.endsWith('/tour')) {
