0dc0803ac25f2670c4219dd3a36369d1977c7b52
Phil Manijak <github@exclsr.com>
2015-12-14 21:51:25 -0800

    Refactor: Move 'circle' routes to own module

--- server/app.js
+++ server/app.js
@@ -43,6 +43,7 @@ var authRoutes = require('./back-end/routes/auth');
 var metrics = require('./back-end/routes/metrics');
 var paymentRoutes = require('./back-end/routes/payment');
 var signupRoutes = require('./back-end/routes/signup');
+var circleRoutes = require('./back-end/routes/circle');
 
 var couchSessionStore = require('circle-blvd/couch-session-store');
 
@@ -215,105 +216,8 @@ var defineRoutes = function () {
         ensure.mainframe, 
         send(db.circles.getAll));
 
-    app.post("/data/circle", 
-        ensure.auth, limits.circle, limits.users.circle, function (req, res) {
-        //
-        var circleName = req.body.name;
-        var user = req.user;
+    app.use('/data/circle', circleRoutes.router(app));
 
-        if (!circleName) {
-            var message = "A 'name' property is required, for naming the circle.";
-            return res.status(400).send(message);
-        }
-
-        db.circles.create(circleName, user.email, handle(res));
-    });
-
-    app.post("/data/circle/admin", ensure.mainframe, function (req, res) {
-        var circle = req.body.circle;
-        var admin = req.body.admin;
-
-        if (!admin.email) {
-            var message = "An email address for an administrative user " +
-                "is required when making a circle.";
-            return res.status(400).send(message);
-        }
-
-        db.circles.create(circle.name, admin.email, handle(res));
-    });
-
-    app.put("/data/circle", ensure.mainframe, function (req, res) {
-        var circle = req.body;
-        db.circles.update(circle, handle(res));
-    });
-
-    app.get("/data/circle/:circleId", ensure.circle, function (req, res) {
-        var circleId = req.params.circleId;
-        db.circles.get(circleId, handle(res));
-    });
-
-    app.put("/data/circle/:circleId/name", ensure.circleAdmin, function (req, res) {
-        var data = req.body;
-        var circleId = req.params.circleId;
-        db.circles.get(circleId, guard(res, function (circle) {
-            circle.name = data.name;
-            db.circles.update(circle, handle(res));
-        }));
-    });
-
-    app.put("/data/circle/:circleId/archive", ensure.circleAdmin, function (req, res) {
-        var circleId = req.params.circleId;
-        var isArchived = req.body.isArchived;
-        db.circles.get(circleId, guard(res, function (circle) {
-            circle.isArchived = isArchived;
-            db.circles.update(circle, handle(res));
-        }));
-    });
-
-    app.put("/data/circle/:circleId/colors/mileposts", ensure.circleAdmin, function (req, res) {
-        var data = req.body;
-        var circleId = req.params.circleId;
-        db.circles.get(circleId, guard(res, function (circle) {
-            circle.colors = circle.colors || {};
-            circle.colors.mileposts = {
-                foreground: data.foreground || "",
-                background: data.background || ""
-            };
-            db.circles.update(circle, handle(res));
-        }));
-    });
-
-    // Invites!
-    app.post("/data/circle/:circleId/invite", ensure.circleAdmin, function (req, res) {
-        var data = req.body;
-
-        var invite = {
-            circleId: req.params.circleId,
-            count: 1,
-            name: data.name
-        };
-
-        if (data.email) {
-            invite.email = data.email;
-        }
-
-        db.invites.create(invite, guard(res, function (dbInvite) {
-            if (!dbInvite.email) {
-                res.status(200).send(dbInvite);
-                return;
-            }
-
-            // Send a notification (email) to the person invited.
-            var params = {
-                user: req.user,
-                invite: dbInvite
-            };
-
-            notify.invitation(params, req, guard(res, function () {
-                res.status(200).send(dbInvite);
-            }));
-        }));
-    });
 
     app.get("/data/invite/:inviteId", function (req, res) {
         var inviteId = req.params.inviteId;
---
+++ server/back-end/routes/circle.js
@@ -0,0 +1,124 @@
+// Routes for circle creation, settings
+// /data/circle
+
+var express = require('express');
+var router = express.Router();
+
+var db = require('circle-blvd/dataAccess').instance();
+var errors = require('circle-blvd/errors');
+var ensure = require('circle-blvd/auth-ensure');
+
+var guard = errors.guard;
+var handle = require('circle-blvd/handle');
+var limits = require('circle-blvd/limits');
+var notify = require('circle-blvd/notify');
+
+router.post("/", 
+    ensure.auth, limits.circle, limits.users.circle, function (req, res) {
+    //
+    var circleName = req.body.name;
+    var user = req.user;
+
+    if (!circleName) {
+        var message = "A 'name' property is required, for naming the circle.";
+        return res.status(400).send(message);
+    }
+
+    db.circles.create(circleName, user.email, handle(res));
+});
+
+router.post("/admin", ensure.mainframe, function (req, res) {
+    var circle = req.body.circle;
+    var admin = req.body.admin;
+
+    if (!admin.email) {
+        var message = "An email address for an administrative user " +
+            "is required when making a circle.";
+        return res.status(400).send(message);
+    }
+
+    db.circles.create(circle.name, admin.email, handle(res));
+});
+
+router.put("/", ensure.mainframe, function (req, res) {
+    var circle = req.body;
+    db.circles.update(circle, handle(res));
+});
+
+router.get("/:circleId", ensure.circle, function (req, res) {
+    var circleId = req.params.circleId;
+    db.circles.get(circleId, handle(res));
+});
+
+router.put("/:circleId/name", ensure.circleAdmin, function (req, res) {
+    var data = req.body;
+    var circleId = req.params.circleId;
+    db.circles.get(circleId, guard(res, function (circle) {
+        circle.name = data.name;
+        db.circles.update(circle, handle(res));
+    }));
+});
+
+router.put("/:circleId/archive", ensure.circleAdmin, function (req, res) {
+    var circleId = req.params.circleId;
+    var isArchived = req.body.isArchived;
+    db.circles.get(circleId, guard(res, function (circle) {
+        circle.isArchived = isArchived;
+        db.circles.update(circle, handle(res));
+    }));
+});
+
+router.put("/:circleId/colors/mileposts", ensure.circleAdmin, function (req, res) {
+    var data = req.body;
+    var circleId = req.params.circleId;
+    db.circles.get(circleId, guard(res, function (circle) {
+        circle.colors = circle.colors || {};
+        circle.colors.mileposts = {
+            foreground: data.foreground || "",
+            background: data.background || ""
+        };
+        db.circles.update(circle, handle(res));
+    }));
+});
+
+// Invites!
+router.post("/:circleId/invite", ensure.circleAdmin, function (req, res) {
+    var data = req.body;
+
+    var invite = {
+        circleId: req.params.circleId,
+        count: 1,
+        name: data.name
+    };
+
+    if (data.email) {
+        invite.email = data.email;
+    }
+
+    db.invites.create(invite, guard(res, function (dbInvite) {
+        if (!dbInvite.email) {
+            res.status(200).send(dbInvite);
+            return;
+        }
+
+        // Send a notification (email) to the person invited.
+        var params = {
+            user: req.user,
+            invite: dbInvite
+        };
+
+        notify.invitation(params, req, guard(res, function () {
+            res.status(200).send(dbInvite);
+        }));
+    }));
+});
+
+
+module.exports = function () {
+    return {
+        router: function (a) {
+            app = a;
+            return router;
+        }
+    }
+}(); // closure
