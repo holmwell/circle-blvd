0af5f651e25365541fdcd36cb380e6f1a2849119
Phil Manijak <github@exclsr.com>
2016-02-10 22:32:17 -0800

    Refactor: Clean up server.js
    Make things a little nicer and more
    obvious upon first glance.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,11 @@
 // server.js
-// TODO: Put all of these in a more robust
-// config situation.
-process.env.PORT = process.env.PORT || 3000;
-process.env.SSL_PORT = process.env.SSL_PORT || 4000;
-process.env.DATABASE_NAME = 'circle-blvd';
-
+// 
+// The main entry point for Circle Blvd. Handles
+// command-line arguments and the highest level
+// config for the app.
+// 
+// 
 var app = require('./app.js');
-
-var start = function () {
-    app.startServer();
-};
 
 // Process command-line arguments
 var isDebugging = false;
@@ -19,8 +15,26 @@ for (var index in process.argv) {
     }
 }
 
-app.init({
-    isDebugging: isDebugging
-});
+// Process environment variables, for now
+// 
+// TODO: Put all of these in a more robust
+// config situation, and get out of messing
+// with process.env so that we can have a 
+// healthy test environment.
+process.env.PORT = process.env.PORT || 3000;
+process.env.SSL_PORT = process.env.SSL_PORT || 4000;
+process.env.DATABASE_NAME = 'circle-blvd';
 
+var config = {
+    isDebugging: isDebugging
+};
+
+// Start the app.
+app.init(config);
+
+// Wait for the database and such to initialize
 app.whenReady(start);
+
+function start () {
+    app.startServer();
+}
