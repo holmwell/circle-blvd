360e5591a7acba3eac8a6f242c48044a7a4b3e06
Phil Manijak <github@exclsr.com>
2016-02-13 15:48:49 -0800

    Refactor: Move static middleware into own module

--- server/app.js
+++ server/app.js
@@ -1,21 +1,18 @@
 // app.js
 var express = require('express');
 var http    = require('http');
-var path    = require('path');
 var io      = require('socket.io')();
 
 // routes
-var router = require('./router.js');
+var router       = require('./router.js');
+var staticRouter = require('./static.js');
 
 // express middleware
 var compression    = require('compression');
-var serveStatic    = require('serve-static');
 var logger         = require('morgan');
 var cookieParser   = require('cookie-parser');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-
-var compactModule = require('compact-exclsr');
 
 var auth   = require('circle-blvd/auth-local');
 var errors = require('circle-blvd/errors');
@@ -114,84 +111,7 @@ var configureApp = function (config, callback) {
         });
     }
 
-    var staticPath = path.join(__dirname, './front-end/public');
-    var minJsPath = '/_js';
-
-    // for minifying JavaScript
-    var compact = compactModule.createCompact({
-        srcPath: staticPath,
-        destPath: path.join(staticPath, minJsPath),
-        webPath: minJsPath,
-        debug: isDebugging
-    });
-
-    compact.addNamespace('lib')
-        .addJs('lib/angular/angular.js')
-        .addJs('lib/angular/angular-route.js')
-        .addJs('lib/angular/angular-sanitize.js')
-        .addJs('lib/store/store.min.js')
-        .addJs('lib/autosize/jquery.autosize.min.js')
-        .addJs('lib/typeahead/0.10.2.js');
-
-    compact.addNamespace('app')
-        .addJs('main/app.js');
-
-    compact.addNamespace('services')
-        .addJs('services/analytics.js')
-        .addJs('services/lib.js')
-        .addJs('services/hacks.js')
-        .addJs('services/signInName.js')
-        .addJs('services/session.js')
-        .addJs('services/stories.js')
-        .addJs('services/errors.js')
-        .addJs('main/services.js');
-
-    compact.addNamespace('controllers')
-        .addJs('ui/controllers/topLevel.js')
-        .addJs('main/controllers.js')
-        .addJs('ui/controllers/story.js')
-        .addJs('ui/controllers/storyList.js')
-        .addJs('ui/controllers/storySummary.js')
-        .addJs('ui/controllers/roadmapMilepost.js')
-        .addJs('ui/controllers/home.js')
-        .addJs('ui/controllers/welcome.js')
-        .addJs('ui/controllers/signin.js')
-        .addJs('ui/controllers/forgot.js')
-        .addJs('ui/controllers/archive.js')
-        .addJs('ui/controllers/lists.js')
-        .addJs('ui/controllers/listDetail.js')
-        .addJs('ui/controllers/profile.js')
-        .addJs('ui/controllers/invite.js')
-        .addJs('ui/controllers/tour.js')
-        .addJs('ui/controllers/contact.js')
-        .addJs('ui/controllers/partner.js')
-        .addJs('ui/controllers/about.js')
-        .addJs('ui/controllers/privacy.js')
-        .addJs('ui/controllers/donate.js')
-        .addJs('ui/controllers/admin.js')
-        .addJs('ui/controllers/createCircle.js')
-        .addJs('ui/controllers/removeHash.js')
-        .addJs('ui/controllers/mainframe.js')
-        .addJs('ui/controllers/fix.js')
-
-    compact.addNamespace('main')
-        .addJs('main/filters.js')
-        .addJs('main/directives.js')
-
-    // Rudimentary cache handling
-    var version = Date.now().toString();
-    compact.addNamespace(version);
-
-    app.use(serveStatic(staticPath));
-
-    app.use(compact.middleware([
-        'lib', 
-        'app', 
-        'services', 
-        'controllers', 
-        'main',
-        version
-    ]));
+    app.use(staticRouter(isDebugging));
 
     app.use(logger('dev'));
     app.use(cookieParser()); // TODO: Signed cookies?
---
+++ server/static.js
@@ -0,0 +1,94 @@
+// static.js
+//
+// Express router for serving static files 
+//
+var express = require('express');
+var path    = require('path');
+
+var serveStatic   = require('serve-static');
+var compactModule = require('compact-exclsr');
+var jsManifest    = require('./js-client-manifest.js');
+
+var staticPath = path.join(__dirname, './front-end/public');
+var minJsPath = '/_js';
+
+module.exports = function (isDebugging) {
+    var router = express.Router();
+
+    // for minifying JavaScript
+    var compact = compactModule.createCompact({
+        srcPath: staticPath,
+        destPath: path.join(staticPath, minJsPath),
+        webPath: minJsPath,
+        debug: isDebugging
+    });
+
+    compact.addNamespace('lib')
+        .addJs('lib/angular/angular.js')
+        .addJs('lib/angular/angular-route.js')
+        .addJs('lib/angular/angular-sanitize.js')
+        .addJs('lib/store/store.min.js')
+        .addJs('lib/autosize/jquery.autosize.min.js')
+        .addJs('lib/typeahead/0.10.2.js');
+
+    compact.addNamespace('app')
+        .addJs('main/app.js');
+
+    compact.addNamespace('services')
+        .addJs('services/analytics.js')
+        .addJs('services/lib.js')
+        .addJs('services/hacks.js')
+        .addJs('services/signInName.js')
+        .addJs('services/session.js')
+        .addJs('services/stories.js')
+        .addJs('services/errors.js')
+        .addJs('main/services.js');
+
+    compact.addNamespace('controllers')
+        .addJs('ui/controllers/topLevel.js')
+        .addJs('main/controllers.js')
+        .addJs('ui/controllers/story.js')
+        .addJs('ui/controllers/storyList.js')
+        .addJs('ui/controllers/storySummary.js')
+        .addJs('ui/controllers/roadmapMilepost.js')
+        .addJs('ui/controllers/home.js')
+        .addJs('ui/controllers/welcome.js')
+        .addJs('ui/controllers/signin.js')
+        .addJs('ui/controllers/forgot.js')
+        .addJs('ui/controllers/archive.js')
+        .addJs('ui/controllers/lists.js')
+        .addJs('ui/controllers/listDetail.js')
+        .addJs('ui/controllers/profile.js')
+        .addJs('ui/controllers/invite.js')
+        .addJs('ui/controllers/tour.js')
+        .addJs('ui/controllers/contact.js')
+        .addJs('ui/controllers/partner.js')
+        .addJs('ui/controllers/about.js')
+        .addJs('ui/controllers/privacy.js')
+        .addJs('ui/controllers/donate.js')
+        .addJs('ui/controllers/admin.js')
+        .addJs('ui/controllers/createCircle.js')
+        .addJs('ui/controllers/removeHash.js')
+        .addJs('ui/controllers/mainframe.js')
+        .addJs('ui/controllers/fix.js')
+
+    compact.addNamespace('main')
+        .addJs('main/filters.js')
+        .addJs('main/directives.js')
+
+    // Rudimentary cache handling
+    var version = Date.now().toString();
+    compact.addNamespace(version);
+
+    router.use(serveStatic(staticPath));
+    router.use(compact.middleware([
+        'lib', 
+        'app', 
+        'services', 
+        'controllers', 
+        'main',
+        version
+    ]));
+
+    return router;
+};
