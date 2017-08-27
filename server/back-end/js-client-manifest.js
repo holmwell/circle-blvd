87d152cb4bf168a1cf7a739843c28b1cf6156076
Phil Manijak <github@exclsr.com>
2017-08-26 23:33:45 -0700

    Revert: Bring back spStorySummary.js
    This was still being used by the roadmap view.

--- server/back-end/js-client-manifest.js
+++ server/back-end/js-client-manifest.js
@@ -64,7 +64,7 @@ module.exports = {
 //      'ui/directives/spSortableListWrapper.js', // Loaded via webpack
 //      'ui/directives/spStory.js',
         'ui/directives/spStoryList.js',
-//      'ui/directives/spStorySummary.js',
+        'ui/directives/spStorySummary.js',
         'ui/directives/spRoadmapMilepost.js'
     ],
     filters: [
