b3752cb3695dadc00a8c4f4d98d101cce62c5ab3
Phil Manijak <github@exclsr.com>
2017-09-04 17:29:36 -0700

    Build: Separate webpack.config.js, add watch step

--- server/back-end/webpack.js
+++ server/back-end/webpack.js
@@ -9,6 +9,7 @@ var util    = require('util')
 
 var webpack    = require('webpack');
 var jsManifest = require('./js-client-manifest.js');
+var config     = require('./webpack.config.js');
 
 var minJsPath = '/_js';
 
@@ -17,68 +18,6 @@ module.exports = function (staticPath, isDebugging) {
     // var router = express.Router();
 
     var build = function (callback) {
-        var outputPath = '/_dist';
-        // All of our Vue stuff is in the story list
-        var appEntry = "../front-end/public/ui/directives/spSortableListWrapper.js";
-
-        var fullOutputPath = path.join(staticPath, outputPath);
-        if (fs.existsSync(fullOutputPath) && !isDebugging) {
-            // return next();
-        }
-
-        var module = {
-            rules: [{
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                  loaders: {
-                    // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-                    // the "scss" and "sass" values for the lang attribute to the right configs here.
-                    // other preprocessors should work out of the box, no loader config like this necessary.
-                    'scss': 'vue-style-loader!css-loader!sass-loader',
-                    'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
-                    js: 'babel-loader'
-                  }
-                  // other vue-loader options go here
-                }
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]?[hash]'
-                }
-            },
-            {
-                test: /\.svg$/,
-                loader: 'svg-inline-loader'
-            }]
-        };
-
-        var resolveLoader = {
-            modules: [path.resolve(__dirname, '../node_modules')]
-        };
-
-        var config = [{
-            stats: "errors-only",
-            entry: path.join(__dirname, '../front-end/entry.js'),
-            output: {
-                path: path.join(staticPath, outputPath),
-                filename: 'circle.js'
-            },
-            module: module,
-            resolveLoader: resolveLoader
-        },{
-            entry: path.join(__dirname, appEntry),
-            context: path.resolve(__dirname, "../front-end"),
-            output: {
-                path: path.join(staticPath, outputPath)
-            },
-            module: module,
-            resolveLoader: resolveLoader
-        }
-        ];
-
         if (!isDebugging) {
             for (var index in config) {
                 config[index].plugins = [
