c71ff4b5bdf031bef07e590a9bc4e1d424b30a82
Phil Manijak <github@exclsr.com>
2018-03-31 00:56:27 -0700

    IO: Update story status via Slack message
    Very much work-in-progress, but it proves out
    the concept.

--- server/back-end/routes/io.js
+++ server/back-end/routes/io.js
@@ -27,80 +27,92 @@ module.exports = function (db) {
 				return;
 			}
 
-			//console.log(payload);
+			// console.log("Payload:");
+			// console.log(payload);
+
 			var action = payload.actions.shift();
 
 			message.attachments[0] = {
 				title: message.attachments[0].title,
 				text: '<@' + payload.user.id + '> ' +
-				'marked this task as *' + action.value + '*'
+				'marked this task as *' + action.name + '*'
 			};
 
 			res.status(200).send(message);
 
-			if (action.value !== "On it" && action.value !== "Will do") {
-				return;
-			}
+			db.docs.get(payload.callback_id, function (err, story) {
+				if (!story.owner) {
+					story.owner = payload.user.name;
+				}
+				story.status = action.value;
+				db.stories.save(story, afterStorySave);
+			});
 
-			// Send more messages to Slack if needed
-			db.circles.getBySlackTeamId(payload.team.id, function (err, circle) {
-				if (err) {
-					errors.log(err);
+			function afterStorySave() {
+				if (action.name !== "On it" && action.name !== "Will do") {
 					return;
 				}
 
-				var dm = {
-					token: circle.access.slack.bot.bot_access_token
-				};
+				// Send more messages to Slack if needed
+				db.circles.getBySlackTeamId(payload.team.id, function (err, circle) {
+					if (err) {
+						errors.log(err);
+						return;
+					}
 
-				db.docs.get(payload.callback_id, function (err, story) {
-					request.post('https://slack.com/api/im.open', {
-						json: true,
-						form: {
-							token: dm.token,
-							user: payload.user.id
-						}
-					}, function (err, response, body) {
-						if (body.ok) {
-							request.post('https://slack.com/api/chat.postMessage', {
-								json: true,
-								form: {
-									token: dm.token,
-									channel: body.channel.id,
-									text: "When you're done with this task, please let us know",
-									attachments: JSON.stringify([{
-										title: story.summary,
-										fallback: "",
-										callback_id: payload.callback_id,
-										attachment_type: "default",
-										actions: [
-										{
-											name: "status",
-											style: "primary",
-											text: "Done",
-											type: "button",
-											value: "Done"
-										},{
-											name: "status",
-											text: "Help?",
-											type: "button",
-											value: "Help?"
-										}]
-									}])
-								}
-							}, function (err, response, body) {
+					var dm = {
+						token: circle.access.slack.bot.bot_access_token
+					};
+
+					db.docs.get(payload.callback_id, function (err, story) {
+						request.post('https://slack.com/api/im.open', {
+							json: true,
+							form: {
+								token: dm.token,
+								user: payload.user.id
+							}
+						}, function (err, response, body) {
+							if (body.ok) {
+								request.post('https://slack.com/api/chat.postMessage', {
+									json: true,
+									form: {
+										token: dm.token,
+										channel: body.channel.id,
+										text: "When you're done with this task, please let us know",
+										attachments: JSON.stringify([{
+											title: story.summary,
+											fallback: "",
+											callback_id: payload.callback_id,
+											attachment_type: "default",
+											actions: [
+											{
+												name: "Done",
+												style: "primary",
+												text: "Done",
+												type: "button",
+												value: "done"
+											},{
+												name: "Not going to happen",
+												text: "Not going to happen",
+												type: "button",
+												value: "sad"
+											}]
+										}])
+									}
+								}, function (err, response, body) {
+									console.log(err);
+									console.log(body);
+								});
+							}
+							else {
 								console.log(err);
 								console.log(body);
-							});
-						}
-						else {
-							console.log(err);
-							console.log(body);
-						}
-					});
+							}
+						});
 
+					});
 				});
-			});
+			}
 		}
 	});
 
